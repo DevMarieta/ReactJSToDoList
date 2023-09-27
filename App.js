9206eff449f2ddc98b592ce973380e3788726917
Mariya Hadzhiyska <136964468+DevMarieta@users.noreply.github.com>
2023-09-27 15:52:19 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,47 +1,61 @@
-.App {
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  width: 100vw;
-  font-family: Arial, Helvetica, sans-serif;
+import "./App.css";
+import { useState } from "react";
+import { Task } from "./Task";
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList(task.taskName !== "" ? [...todoList, task] : todoList);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}> Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-body {
-  margin: 0;
-  padding: 0;
-}
-
-.addTask {
-  height: 30vh;
-  background-color: dodgerblue;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.list {
-  height: auto;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  padding-top: 50px;
-  flex-direction: column;
-}
-
-.task {
-  width: 500px;
-  height: 50px;
-  display: flex;
-  margin: 15px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.task button {
-  width: auto;
-  height: 20px;
-  margin-left: 5px;
-  text-align: center;
-}
+export default App;
